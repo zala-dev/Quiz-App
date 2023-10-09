3980414df82397073ae927563b2749de0640b6cb
Bhagirath <zala@Bhagiraths-MacBook-Pro.local>
2023-10-09 19:21:25 +1030

    app development complete

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header className="app-header">
+      <img src="quiz.png" alt="React logo" />
+      <h1>The React Quiz</h1>
+    </header>
+  );
+}
+
+export default Header;
